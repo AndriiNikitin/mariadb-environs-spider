1014e08654750a2b47f297799e32edca8016ea05
AndriiNikitin <AndriiNikitin@users.noreply.github.com>
2017-07-07 21:33:47 +0200

    Update example_in_docker.sh

--- _script/example_in_docker.sh
+++ _script/example_in_docker.sh
@@ -1,7 +1,7 @@
 tee Dockerfile <<EOF
 from centos:7
 
-RUN yum -y install m4 git wget python cmake make gcc-c++ ncurses-devel bison zlib zlib-devel zlib-static openssl vim findutils openssl vim m4 libaio libnuma numactl
+RUN yum -y install m4 git wget python cmake make gcc-c++ ncurses-devel bison zlib zlib-devel zlib-static openssl vim findutils openssl vim m4 libaio libnuma numactl gnutls-devel
 
 RUN wget -O example.sh https://raw.github.com/AndriiNikitin/mariadb-environs-spider/master/_script/example.sh
 
