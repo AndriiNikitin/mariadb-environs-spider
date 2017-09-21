2735773f5f52c8770d00ef8af3af6fd5dc6c6eb9
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-09-21 12:08:37 +0200

    add support for hash partition

--- _script/create_table_filter.py
+++ _script/create_table_filter.py
@@ -16,15 +16,22 @@ import subprocess
 # uncomment line below for detailed debugging
 # logging.basicConfig(format=LOGFORMAT, level=logging.DEBUG)
 
+node_list_file = None
+
 
 def print_next_environ_binding(next_environ_binding):
-        portcmd = "grep port m{}*/gen_cnf.sh | head -n 1".format(next_environ_binding)
+	if node_list_file:
+		portcmd = "grep port $(tail -n+{} '{}' | head -n1)*/gen_cnf.sh | head -n 1".format(next_environ_binding,node_list_file)
+		hostcmd = "grep hostname $(tail -n+{} '{}' | head -n1)*/gen_cnf.sh | head -n 1".format(next_environ_binding,node_list_file)
+	else:
+		portcmd = "grep port m{}*/gen_cnf.sh | head -n 1".format(next_environ_binding)
+		hostcmd = "grep hostname m{}*/gen_cnf.sh | head -n 1".format(next_environ_binding)
+
 	port = subprocess.check_output(['bash','-c', portcmd])
-        if not port or port.find("=") == -1 : 
+	if not port or port.find("=") == -1 : 
 		raise ValueError("Couldn't retrieve port value for environ {}".format(next_environ_binding))
 	port = port.split("=")[1].strip()
 
-        hostcmd = "grep hostname m{}*/gen_cnf.sh | head -n 1".format(next_environ_binding)
 	host = subprocess.check_output(['bash','-c', hostcmd])
 
 	if not host : 
@@ -86,7 +93,7 @@ def parse_create_table(is_root_node):
 
 
 		if is_root_node and state == States.IN_CLAUSE_PARTITION_BY and line:
-			m = re.match(r'([^\;]*)(PARTITION\s+)([^\s,\'\"]*)(\s+VALUES\s+)([^,\;\n]*)(,)?(.*)', line, re.I)
+			m = re.match(r'([^\;]*)(PARTITION\s+)([^\s,\'\"\)]*)(\s+VALUES\s+)?([^,\;\n]*)?(,)?(.*)', line, re.I)
 			if m:
 				if m.group(1) : sys.stdout.write(m.group(1))
 				if m.group(2) : sys.stdout.write(m.group(2))
@@ -99,7 +106,6 @@ def parse_create_table(is_root_node):
 				if m.group(7) : line = m.group(7) + line
 				if m.group(6) : line = m.group(6) + line
 
-
 		if line:
 			if not ( not is_root_node and state == States.IN_CLAUSE_PARTITION_BY) : 
 				sys.stdout.write(line)
@@ -110,6 +116,9 @@ def parse_create_table(is_root_node):
 			cached_engine_definition = ""
 			next_environ_idx=0
 			state = States.OUTSIDE
+
+if len(sys.argv)>2 :
+	node_list_file = sys.argv[2]
 
 
 parse_create_table(len(sys.argv)>1 and sys.argv[1]=="0")
